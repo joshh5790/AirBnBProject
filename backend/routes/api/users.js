0085cea2a38361023041b0e4333d011f15ac8c5a
Joshua Ho <joshh5790@gmail.com>
2023-06-26 17:23:57 -0700

    add user nput valdation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,8 +3,32 @@ const router = express.Router()
 const bcrypt = require('bcryptjs')
 const { setTokenCookie, requireAuth } = require('../../utils/auth')
 const { User } = require('../../db/models')
+const { check } = require('express-validator')
+const { handleValidationErrors } = require('../../utils/validation')
 
-router.post('/', async (req, res, next) => {
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true})
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
+    check('username')
+        .exists({ checkFalsy: true})
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
+    check('password')
+        .exists({ checkFalsy: true})
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+]
+
+
+// sign up!
+router.post('/', validateSignup, async (req, res, next) => {
     const { email, password, username } = req.body
     const hashedPassword = bcrypt.hashSync(password)
     const user = await User.create({ email, username, hashedPassword })
