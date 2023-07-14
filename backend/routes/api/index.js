353acc1bf567398d95278cb080fdf0dd38dc31a0
Joshua Ho <joshh5790@gmail.com>
2023-07-13 17:33:25 -0700

    complete image delete endpoints

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -10,6 +10,8 @@ router.use('/session', require('./session'))
 router.use('/spots', require('./spots'))
 router.use('/reviews', reviews.router)
 router.use('/bookings', require('./bookings'))
+router.use('/spot-images', require('./spot-images'))
+router.use('/review-images', require('./review-images'))
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
