5c7116d242651761e6c77954e7a048b1b5ae1995
Joshua Ho <joshh5790@gmail.com>
2023-07-17 17:41:31 -0700

    minor fix

--- backend/db/migrations/4-create-reviews.js
+++ backend/db/migrations/4-create-reviews.js
@@ -53,21 +53,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-<<<<<<< HEAD
     }, options);
-    // await queryInterface.addConstraint('Reviews', {
-    //   fields: ['userId', 'spotId'],
-    //   type: 'unique',
-    //   name: 'unique_user_review',
-    //   options
-=======
-    });
-    // await queryInterface.addConstraint('Reviews', {
-    //   fields: ['userId', 'spotId'],
-    //   type: 'unique',
-    //   name: 'unique_user_review'
->>>>>>> dev
-    // })
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Reviews"
