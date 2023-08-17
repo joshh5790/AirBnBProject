3ccb575d553e884b87f81698ec7a994516d1ab12
Joshua Ho <joshh5790@gmail.com>
2023-08-17 16:47:33 -0700

    phase 1 of frontend setup complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,29 @@
+import LoginFormPage from "./components/LoginFormPage";
+import { Route, Switch } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import * as sessionActions from "./store/session";
+
 function App() {
-  return (
-    <h1>Hello from App</h1>
-  );
+  const dispatch = useDispatch()
+  const [isLoaded, setIsLoaded] = useState(false)
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser())
+    .then(() => setIsLoaded(true))
+  })
+
+  return (isLoaded && (
+    <div>
+      <h1>Hello from App</h1>
+      <NavLink exact to='/login'>Login</NavLink>
+      <Switch>
+        <Route exact path='/login'>
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    </div>
+  ));
 }
 
 export default App;
