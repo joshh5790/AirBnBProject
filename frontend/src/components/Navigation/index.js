88da27848534187781fc16e2bd377f3753d232bf
Joshua Ho <joshh5790@gmail.com>
2023-08-19 13:27:08 -0700

    nest login and sign up in profile button

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,41 +2,19 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import OpenModalButton from '../OpenModalButton';
-import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
-import SignupFormModal from '../SignupFormModal';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <li>
-        <ProfileButton user={sessionUser} />
-      </li>
-    );
-  } else {
-    sessionLinks = (
-      <li>
-        <OpenModalButton
-          buttonText='Log In'
-          modalComponent={<LoginFormModal />}
-        />
-        <OpenModalButton
-          buttonText='Sign Up'
-          modalComponent={<SignupFormModal />}
-        />
-      </li>
-    );
-  }
 
   return (
     <ul>
       <li>
         <NavLink exact to="/">Home</NavLink>
       </li>
-      {isLoaded && sessionLinks}
+      {isLoaded && <li>
+        <ProfileButton user={sessionUser} />
+      </li>}
     </ul>
   );
 }
