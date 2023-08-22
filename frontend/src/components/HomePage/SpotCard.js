3b434442076e02e797bd433bb036b1ce332fed56
Joshua Ho <joshh5790@gmail.com>
2023-08-22 00:37:28 -0700

    add tooltip and responsive landing page

--- frontend/src/components/HomePage/SpotCard.js
+++ frontend/src/components/HomePage/SpotCard.js
@@ -1,10 +1,41 @@
 import './SpotCard.css'
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 
 const SpotCard = ({ spot }) => {
+    const [isVisible, setIsVisible] = useState(false)
+    const [position, setPosition] = useState({ x: 0, y: 0 })
+
+    const handleMouseEnter = (e) => {
+        setIsVisible(true);
+        updateTooltipPosition(e);
+    };
+
+    const handleMouseMove = (e) => {
+        updateTooltipPosition(e);
+    };
+
+    const handleMouseLeave = () => {
+        setIsVisible(false);
+    };
+
+    const updateTooltipPosition = (e) => {
+        const currCard = e.currentTarget.getBoundingClientRect()
+        setPosition({
+            x: e.clientX - currCard.left + 10,
+            y: e.clientY - currCard.top + 10
+        });
+    };
+
     return (
-        <NavLink to={`/spots/${spot.id}`}>
-            <div className="spot-card">
+        <NavLink
+            to={`/spots/${spot.id}`}
+            className="spot-card"
+            onMouseEnter={handleMouseEnter}
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+        >
+            <div>
                 <img
                     src={spot.previewImage || 'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg'}
                     alt={spot.previewImage || 'Image not found'}
@@ -18,6 +49,11 @@ const SpotCard = ({ spot }) => {
                 </div>
                 <div className='price'><span className='price-num'>{`$${spot.price} `}</span>night</div>
             </div>
+            {isVisible && (
+                <div className="tooltip" style={{ left: position.x, top: position.y }}>
+                    {spot.name}
+                </div>
+            )}
         </NavLink>
     )
 }
